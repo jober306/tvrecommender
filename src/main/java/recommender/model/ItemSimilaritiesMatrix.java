9604b032eba52abe9964b268c3ab4f0469bbbec7
Jonathan Bergeron <jonathan.bergeron.6@ulaval.ca>
2016-09-29 12:58:32 -0400

    Commented the item and user similarity matrix class.

--- src/main/java/recommender/model/ItemSimilaritiesMatrix.java
+++ src/main/java/recommender/model/ItemSimilaritiesMatrix.java
@@ -2,19 +2,37 @@ package recommender.model;
 
 import recommender.similarities.Similarity;
 
+/**
+ * Class the represents an item similarity matrix.
+ * 
+ * @author Jonathan Bergeron
+ *
+ */
 public class ItemSimilaritiesMatrix extends SimilarityMatrix {
-	
-	public ItemSimilaritiesMatrix(UserItemMatrix userItemMatrix, Similarity similarity){
+
+	/**
+	 * Constructor of the class. It uses an user item matrix and a similarity to
+	 * calculate its entries.
+	 * 
+	 * @param userItemMatrix
+	 *            The <class>UserItemMatrix</class> from which the similarities
+	 *            are calculated.
+	 * @param similarity
+	 *            The similarity to be used.
+	 */
+	public ItemSimilaritiesMatrix(UserItemMatrix userItemMatrix,
+			Similarity similarity) {
 		this.similarity = similarity;
 		calculateItemSimilaritiesMatrix(userItemMatrix);
 	}
-	
-	private void calculateItemSimilaritiesMatrix(UserItemMatrix userItemMatrix){
+
+	private void calculateItemSimilaritiesMatrix(UserItemMatrix userItemMatrix) {
 		int nbItems = userItemMatrix.getNumberOfItems();
 		similaritiesMatrix = new double[nbItems][nbItems];
-		for(int i = 0; i < nbItems; i++){
-			for(int j = 0; j < nbItems; j++){
-				similaritiesMatrix[i][j] = userItemMatrix.getItemsSimilarity(i, j, similarity);
+		for (int i = 0; i < nbItems; i++) {
+			for (int j = 0; j < nbItems; j++) {
+				similaritiesMatrix[i][j] = userItemMatrix.getItemsSimilarity(i,
+						j, similarity);
 			}
 		}
 	}
--- src/main/java/recommender/model/UserSimilaritiesMatrix.java
+++ src/main/java/recommender/model/UserSimilaritiesMatrix.java
@@ -2,19 +2,37 @@ package recommender.model;
 
 import recommender.similarities.Similarity;
 
-public class UserSimilaritiesMatrix extends SimilarityMatrix{
-	
-	public UserSimilaritiesMatrix(UserItemMatrix userItemMatrix, Similarity similarity){
+/**
+ * Class the represents an user similarity matrix.
+ * 
+ * @author Jonathan Bergeron
+ *
+ */
+public class UserSimilaritiesMatrix extends SimilarityMatrix {
+
+	/**
+	 * Constructor of the class. It uses an user item matrix and a similarity to
+	 * calculate its entries.
+	 * 
+	 * @param userItemMatrix
+	 *            The <class>UserItemMatrix</class> from which the similarities
+	 *            are calculated.
+	 * @param similarity
+	 *            The similarity to be used.
+	 */
+	public UserSimilaritiesMatrix(UserItemMatrix userItemMatrix,
+			Similarity similarity) {
 		this.similarity = similarity;
 		calculateUserSimilaritiesMatrix(userItemMatrix);
 	}
-	
-	private void calculateUserSimilaritiesMatrix(UserItemMatrix userItemMatrix){
+
+	private void calculateUserSimilaritiesMatrix(UserItemMatrix userItemMatrix) {
 		int nbUsers = userItemMatrix.getNumberOfUsers();
 		similaritiesMatrix = new double[nbUsers][nbUsers];
-		for(int i = 0; i < nbUsers; i++){
-			for(int j = 0; j < nbUsers; j++){
-				similaritiesMatrix[i][j] = userItemMatrix.getUsersSimilarity(i, j, similarity);
+		for (int i = 0; i < nbUsers; i++) {
+			for (int j = 0; j < nbUsers; j++) {
+				similaritiesMatrix[i][j] = userItemMatrix.getUsersSimilarity(i,
+						j, similarity);
 			}
 		}
 	}
