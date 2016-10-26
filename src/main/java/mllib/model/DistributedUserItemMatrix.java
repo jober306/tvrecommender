bcc73bac7fb278dccdbc31a73b308834f0eb21a6
H. C <Hamidreza.Chinaei.1@ulaval.ca>
2016-10-26 14:45:55 -0500

    Added two getter methods to the distributed user item matrix class and tested the class.

--- src/main/java/mllib/model/DistributedUserItemMatrix.java
+++ src/main/java/mllib/model/DistributedUserItemMatrix.java
@@ -1,5 +1,7 @@
 package mllib.model;
 
+import java.util.List;
+
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.mllib.linalg.distributed.CoordinateMatrix;
 import org.apache.spark.mllib.linalg.distributed.IndexedRow;
@@ -23,6 +25,22 @@ public class DistributedUserItemMatrix {
 		data = new IndexedRowMatrix(rows.rdd());
 	}
 	
+	public IndexedRow getRow(int rowIndex){
+		List<IndexedRow> rows =  data.rows().toJavaRDD().filter(indexedRow -> indexedRow.index() == rowIndex).collect();
+		if(rows.size() == 1){
+			return rows.get(0);
+		}
+		return null;
+	}
+	
+	public double getValue(int rowIndex, int columnIndex){
+		return getRow(rowIndex).vector().toArray()[columnIndex];
+	}
+	
+	/**
+	 * Wrapper method that return cosine similarity between columns.
+	 * @return A coordinate matrix containing similarities between columns.
+	 */
 	public CoordinateMatrix getItemSimilarities(){
 		return data.columnSimilarities();
 	}
---
+++ src/test/java/mllib/model/DistributedUserItemMatrixTest.java
@@ -0,0 +1,74 @@
+package mllib.model;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.linalg.SparseVector;
+import org.apache.spark.mllib.linalg.Vectors;
+import org.apache.spark.mllib.linalg.distributed.IndexedRow;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import spark.utilities.SparkUtilities;
+
+public class DistributedUserItemMatrixTest {
+	
+	private static final int[][] matrixIndices = {{0,3,4,7,8},{},{1,2,3}};
+	private static final double[][] matrixValues = {{1.0d,2.0d,5.0d,2.0d,1.0d},{},{3.0d,2.0d,4.0d}};
+	private static final int matrixNumberOfColumn = 10;
+	
+	static DistributedUserItemMatrix R;
+	static JavaRDD<IndexedRow> rows;
+	static JavaSparkContext sc;
+	
+	@BeforeClass
+	public static void setUp(){
+		sc = SparkUtilities.getADefaultSparkContext();
+		List<IndexedRow> rowList = new ArrayList<IndexedRow>();
+		for(int i = 0; i < matrixIndices.length; i++){
+			rowList.add(new IndexedRow(i, Vectors.sparse(matrixNumberOfColumn, matrixIndices[i], matrixValues[i])));
+		}
+		rows = SparkUtilities.<IndexedRow>elementsToJavaRDD(rowList, sc);
+		R = new DistributedUserItemMatrix(rows);
+	}
+	
+	@Test
+	public void getRowTest() {
+		for(int row = 0; row < matrixIndices.length; row++){
+			IndexedRow indexedRow = R.getRow(row);
+			SparseVector sparseVec = indexedRow.vector().toSparse();
+			assertArrayEquals(sparseVec.indices(), matrixIndices[row]);
+			assertArrayEquals(sparseVec.values(), matrixValues[row], 0.0d);
+		}
+	}
+	
+	@Test
+	public void getValueTest(){
+		for(int row = 0; row < matrixIndices.length; row++){
+			ArrayList<Integer> indices = new ArrayList<Integer>(Arrays.asList(ArrayUtils.toObject(matrixIndices[row])));
+			for(int col = 0; col < matrixNumberOfColumn; col++){
+				int valueIndex = indices.indexOf(col);
+				if(valueIndex == -1){
+					assertEquals(0.0d, R.getValue(row, col),0.0d);
+				}
+				else{
+					assertEquals(matrixValues[row][valueIndex], R.getValue(row, col), 0.0d);
+				}
+			}
+		}
+	}
+	
+	@AfterClass
+	public static void tearDownOnce(){
+		sc.close();
+	}
+
+}
