10c9d90f6c41a4442edf08db9cc07934f2118ce2
Jonathan Bergeron <jonathan.bergeron.6@ulaval.ca>
2016-11-03 09:56:45 -0400

    Added test to the hard thresholding method for a diagonal matrix in vector representation.

--- src/main/java/spark/utilities/DistributedMatrixUtilities.java
+++ src/main/java/spark/utilities/DistributedMatrixUtilities.java
@@ -7,7 +7,6 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.lang3.ArrayUtils;
-import org.apache.spark.api.java.JavaDoubleRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.mllib.linalg.Matrices;
 import org.apache.spark.mllib.linalg.Matrix;
@@ -16,165 +15,216 @@ import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.Vectors;
 import org.apache.spark.mllib.linalg.distributed.IndexedRow;
 import org.apache.spark.mllib.linalg.distributed.IndexedRowMatrix;
-import org.apache.spark.sql.catalyst.expressions.aggregate.Final;
-import org.apache.spark.sql.execution.vectorized.ColumnarBatch.Row;
 
 import scala.Tuple2;
 import scala.Tuple3;
 
 /**
- * Class that offers multiple utility function on mlllib distributed matrix object.
+ * Class that offers multiple utility function on mlllib distributed matrix
+ * object.
+ * 
  * @author Jonathan Bergeron
  *
  */
 public class DistributedMatrixUtilities {
-	
+
 	/**
 	 * Method that transposes the given matrix.
-	 * @param M The matrix to be transposed.
+	 * 
+	 * @param M
+	 *            The matrix to be transposed.
 	 * @return The transpose of the given matrix.
 	 */
-	public static IndexedRowMatrix transpose(IndexedRowMatrix M){
-		JavaRDD<IndexedRow> transposedMatrix = M.rows().toJavaRDD().<Tuple3<Integer,Integer,Double>>flatMap(row -> mapRowToData(row)).mapToPair(triplet -> new Tuple2<Integer, Tuple2<Integer,Double>>(triplet._2(), new Tuple2<Integer,Double>(triplet._1(),triplet._3())))
-		.aggregateByKey(new ArrayList<Tuple2<Integer,Double>>(), (list, tuple) -> {list.add(tuple); return list;}, (list1,list2)-> {list1.addAll(list2); return list1;})
-		.map(data -> mapDataToRow(data));
+	public static IndexedRowMatrix transpose(IndexedRowMatrix M) {
+		JavaRDD<IndexedRow> transposedMatrix = M
+				.rows()
+				.toJavaRDD()
+				.<Tuple3<Integer, Integer, Double>> flatMap(
+						row -> mapRowToDenseTriplets(row))
+				.mapToPair(
+						triplet -> new Tuple2<Integer, Tuple2<Integer, Double>>(
+								triplet._2(), new Tuple2<Integer, Double>(
+										triplet._1(), triplet._3())))
+				.aggregateByKey(new ArrayList<Tuple2<Integer, Double>>(),
+						(list, tuple) -> {
+							list.add(tuple);
+							return list;
+						}, (list1, list2) -> {
+							list1.addAll(list2);
+							return list1;
+						}).map(data -> mapDataToRow(data));
 		return new IndexedRowMatrix(transposedMatrix.rdd());
 	}
-	
-	private static Iterator<Tuple3<Integer,Integer,Double>> mapRowToData(IndexedRow row){
-		List<Tuple3<Integer,Integer,Double>> values = new ArrayList<Tuple3<Integer,Integer,Double>>();
-		double[] colValues = row.vector().toArray();
-		for(int col = 0; col < colValues.length; col++){
-			values.add(new Tuple3<Integer,Integer,Double>(toIntExact(row.index()), col, colValues[col]));
-		}
-		return values.iterator();
-	}
-	
-	private static IndexedRow mapDataToRow(Tuple2<Integer, ArrayList<Tuple2<Integer,Double>>> data){
-		int rowSize = data._2.size();
-		double[] rowValues = new double[rowSize];
-		for(int i = 0; i <rowSize; i++){
-			rowValues[data._2.get(i)._1] = data._2.get(i)._2;
-		}
-		return new IndexedRow(data._1, Vectors.dense(rowValues).compressed());
-	}
-	
+
 	/**
 	 * Method that inverse a diagonal matrix.
-	 * @param M The diagonal matrix.
+	 * 
+	 * @param M
+	 *            The diagonal matrix.
 	 * @return The inverse of the diagonal matrix.
 	 */
-	public static IndexedRowMatrix inverseDiagonalMatrix(IndexedRowMatrix M){
+	public static IndexedRowMatrix inverseDiagonalMatrix(IndexedRowMatrix M) {
 		JavaRDD<IndexedRow> inverse = M.rows().toJavaRDD().map(row -> {
 			int exactIndex = toIntExact(row.index());
 			double[] data = row.vector().toArray();
-			if(exactIndex <= data.length)
+			if (exactIndex <= data.length)
 				data[exactIndex] = 1.0d / data[exactIndex];
 			return new IndexedRow(row.index(), Vectors.dense(data));
-			});
+		});
 		return new IndexedRowMatrix(inverse.rdd());
 	}
-	
+
 	/**
-	 * Method that applies the hard thresholding operator, i.e. keeping only the top r eigen value, the rest is set to zero.
-	 * @param M The matrix to hard threshold.
-	 * @param r The hard threshold.
+	 * Method that applies the hard thresholding operator to a diagonal matrix,
+	 * i.e. keeping only the top r eigen value, the rest is set to zero.
+	 * IMPORTANT It assumes the eigen values to be in descending order.
+	 * 
+	 * @param M
+	 *            The matrix to hard threshold.
+	 * @param r
+	 *            The hard threshold.
 	 * @return The hard thresholded matrix.
 	 */
-	public static IndexedRowMatrix hardThreshold(IndexedRowMatrix M, int r){
-		JavaRDD<IndexedRow> hardThresholdedMatrix = M.rows().toJavaRDD().map(row -> {
-			int exactIndex = toIntExact(row.index());
-			double[] data = row.vector().toArray();
-			if(exactIndex < r && exactIndex <= data.length)
-				data[exactIndex] = 0.0d;
-			return new IndexedRow(row.index(), Vectors.dense(data));
-			});
-		
+	public static IndexedRowMatrix hardThreshold(IndexedRowMatrix M, int r) {
+		JavaRDD<IndexedRow> hardThresholdedMatrix = M.rows().toJavaRDD()
+				.map(row -> {
+					int exactIndex = toIntExact(row.index());
+					double[] data = row.vector().toArray();
+					if (exactIndex < r && exactIndex <= data.length)
+						data[exactIndex] = 0.0d;
+					return new IndexedRow(row.index(), Vectors.dense(data));
+				});
+
 		return new IndexedRowMatrix(hardThresholdedMatrix.rdd());
 	}
-	
+
 	/**
-	 * Method that applies the hard thresholding operator, i.e. keeping only the top r eigen value, the rest is set to zero.
-	 * @param v The vector to hard threshold.
-	 * @param r The hard threshold.
-	 * @return The hard thresholded vector.
+	 * Method that applies the hard thresholding operator to a diagonal matrix
+	 * represented by a vector. The hard thresholding operation keeps the top r
+	 * singular values. IMPORTANT It assumes the eigen values to be in
+	 * descending order.
+	 * 
+	 * @param v
+	 *            The diagonal matrix represented by a vector.
+	 * @param r
+	 *            The hard threshold value.
+	 * @return The hard thresholded matrix in vector representation.
 	 */
-	public static Vector hardThreshold(Vector v, int r){
+	public static Vector hardThreshold(Vector v, int r) {
 		double[] values = v.toArray();
-		for(int i = r; i < values.length; i++){
+		for (int i = r; i < values.length; i++) {
 			values[i] = 0.0d;
 		}
 		return Vectors.dense(values).compressed();
 	}
-	
-	public static IndexedRowMatrix multiplicateByLeftDiagonalMatrix(Vector diagMatrix, IndexedRowMatrix mat){
+
+	public static IndexedRowMatrix multiplicateByLeftDiagonalMatrix(
+			Vector diagMatrix, IndexedRowMatrix mat) {
 		final double[] diagMatValues = diagMatrix.toArray();
-		JavaRDD<IndexedRow> result = mat.rows().toJavaRDD().map(row -> {
-			double[] rowValues = row.vector().toArray();
-			for(int i = 0; i < rowValues.length; i++){
-				rowValues[i] = rowValues[i] * diagMatValues[toIntExact(row.index())];
-			}
-			return new IndexedRow(row.index(), Vectors.dense(rowValues));
-		});
+		JavaRDD<IndexedRow> result = mat
+				.rows()
+				.toJavaRDD()
+				.map(row -> {
+					double[] rowValues = row.vector().toArray();
+					for (int i = 0; i < rowValues.length; i++) {
+						rowValues[i] = rowValues[i]
+								* diagMatValues[toIntExact(row.index())];
+					}
+					return new IndexedRow(row.index(), Vectors.dense(rowValues));
+				});
 		return new IndexedRowMatrix(result.rdd());
 	}
-	
-	public static IndexedRowMatrix multiplicateByRightDiagonalMatrix(IndexedRowMatrix mat, Vector diagMatrix){
+
+	public static IndexedRowMatrix multiplicateByRightDiagonalMatrix(
+			IndexedRowMatrix mat, Vector diagMatrix) {
 		final double[] diagMatValues = diagMatrix.toArray();
 		JavaRDD<IndexedRow> result = mat.rows().toJavaRDD().map(row -> {
 			double[] rowValues = row.vector().toArray();
-			for(int i = 0; i < rowValues.length; i++){
+			for (int i = 0; i < rowValues.length; i++) {
 				rowValues[i] = rowValues[i] * diagMatValues[i];
 			}
 			return new IndexedRow(row.index(), Vectors.dense(rowValues));
 		});
 		return new IndexedRowMatrix(result.rdd());
 	}
-	
-	public static double scalarProduct(double[] v1, double[] v2){
+
+	public static double scalarProduct(double[] v1, double[] v2) {
 		double total = 0;
-		for(int i = 0; i < v1.length; i++){
+		for (int i = 0; i < v1.length; i++) {
 			total += v1[i] * v2[i];
 		}
 		return total;
 	}
-	
-	public static double scalarProduct(Vector v1, Vector v2){
+
+	public static double scalarProduct(Vector v1, Vector v2) {
 		return scalarProduct(v1.toArray(), v2.toArray());
 	}
-	
-	public static Matrix toSparseLocalMatrix(IndexedRowMatrix mat){
-		List<Tuple3<Integer, Integer, Double>> triplets = mat.rows().toJavaRDD().flatMap(row -> extractTripletFromRow(row)).collect();
+
+	public static Matrix toSparseLocalMatrix(IndexedRowMatrix mat) {
+		List<Tuple3<Integer, Integer, Double>> triplets = mat.rows()
+				.toJavaRDD().flatMap(row -> mapRowToSparseTriplets(row))
+				.collect();
 		int[] rowIndices = new int[triplets.size()];
 		int[] colIndices = new int[triplets.size()];
 		double[] values = new double[triplets.size()];
-		for(int i = 0 ; i < triplets.size(); i++){
+		for (int i = 0; i < triplets.size(); i++) {
 			rowIndices[i] = triplets.get(i)._1();
 			colIndices[i] = triplets.get(i)._2();
 			values[i] = triplets.get(i)._3();
 		}
-		return Matrices.sparse(toIntExact(mat.numRows()), toIntExact(mat.numCols()), rowIndices, colIndices, values);
+		return Matrices.sparse(toIntExact(mat.numRows()),
+				toIntExact(mat.numCols()), rowIndices, colIndices, values);
 	}
-	
-	public static Vector multiplyVectorByMatrix(Vector vec, IndexedRowMatrix mat){
+
+	public static Vector multiplyVectorByMatrix(Vector vec, IndexedRowMatrix mat) {
 		final double[] vecValues = vec.toArray();
-		List<Double> results = mat.rows().toJavaRDD().mapToDouble(row -> scalarProduct(vecValues, row.vector().toArray())).collect();
-		return Vectors.dense(ArrayUtils.toPrimitive(results.toArray(new Double[results.size()]))).compressed();
+		List<Double> results = mat
+				.rows()
+				.toJavaRDD()
+				.mapToDouble(
+						row -> scalarProduct(vecValues, row.vector().toArray()))
+				.collect();
+		return Vectors.dense(
+				ArrayUtils.toPrimitive(results.toArray(new Double[results
+						.size()]))).compressed();
 	}
-	
-	public static Vector indexedRowToVector(IndexedRow row){
+
+	public static Vector indexedRowToVector(IndexedRow row) {
 		return Vectors.dense(row.vector().toArray());
 	}
-	
-	private static Iterator<Tuple3<Integer, Integer,Double>> extractTripletFromRow(IndexedRow row){
+
+	private static IndexedRow mapDataToRow(
+			Tuple2<Integer, ArrayList<Tuple2<Integer, Double>>> data) {
+		int rowSize = data._2.size();
+		double[] rowValues = new double[rowSize];
+		for (int i = 0; i < rowSize; i++) {
+			rowValues[data._2.get(i)._1] = data._2.get(i)._2;
+		}
+		return new IndexedRow(data._1, Vectors.dense(rowValues).compressed());
+	}
+
+	private static Iterator<Tuple3<Integer, Integer, Double>> mapRowToSparseTriplets(
+			IndexedRow row) {
 		List<Tuple3<Integer, Integer, Double>> triplets = new ArrayList<Tuple3<Integer, Integer, Double>>();
 		int rowIndex = toIntExact(row.index());
 		SparseVector sparceRow = row.vector().toSparse();
 		int[] indices = sparceRow.indices();
 		double[] values = sparceRow.values();
-		for(int i = 0; i < indices.length; i++){
-			triplets.add(new Tuple3<Integer, Integer, Double>(rowIndex, indices[i], values[i]));
+		for (int i = 0; i < indices.length; i++) {
+			triplets.add(new Tuple3<Integer, Integer, Double>(rowIndex,
+					indices[i], values[i]));
 		}
 		return triplets.iterator();
+	}
+
+	private static Iterator<Tuple3<Integer, Integer, Double>> mapRowToDenseTriplets(
+			IndexedRow row) {
+		List<Tuple3<Integer, Integer, Double>> values = new ArrayList<Tuple3<Integer, Integer, Double>>();
+		double[] colValues = row.vector().toArray();
+		for (int col = 0; col < colValues.length; col++) {
+			values.add(new Tuple3<Integer, Integer, Double>(toIntExact(row
+					.index()), col, colValues[col]));
+		}
+		return values.iterator();
 	}
 }
--- src/test/java/spark/utilities/DistributedMatrixUtilitiesTest.java
+++ src/test/java/spark/utilities/DistributedMatrixUtilitiesTest.java
@@ -1,15 +1,17 @@
 package spark.utilities;
 
+import static java.lang.Math.toIntExact;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
-import static java.lang.Math.toIntExact;
-
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.linalg.Matrix;
+import org.apache.spark.mllib.linalg.SingularValueDecomposition;
+import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.Vectors;
 import org.apache.spark.mllib.linalg.distributed.IndexedRow;
 import org.apache.spark.mllib.linalg.distributed.IndexedRowMatrix;
@@ -18,70 +20,106 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class DistributedMatrixUtilitiesTest {
-	
-	private static final double[][] matrixValues = {{1.0d,2.0d,5.0d,2.0d},{3.0d,2.0d,4.0d,1.0d}};
-	
+
+	private static final double[][] matrixValues = {
+			{ 1.0d, 2.0d, 5.0d, 2.0d }, { 3.0d, 2.0d, 4.0d, 1.0d } };
+
 	static IndexedRowMatrix R;
 	static JavaRDD<IndexedRow> rows;
 	static JavaSparkContext sc;
-	
+
 	@BeforeClass
-	public static void setUpOnce(){
+	public static void setUpOnce() {
 		sc = SparkUtilities.getADefaultSparkContext();
 		List<IndexedRow> rowList = new ArrayList<IndexedRow>();
-		for(int i = 0; i < matrixValues.length; i++){
+		for (int i = 0; i < matrixValues.length; i++) {
 			rowList.add(new IndexedRow(i, Vectors.dense(matrixValues[i])));
 		}
-		rows = SparkUtilities.<IndexedRow>elementsToJavaRDD(rowList, sc);
+		rows = SparkUtilities.<IndexedRow> elementsToJavaRDD(rowList, sc);
 		R = new IndexedRowMatrix(rows.rdd());
 	}
-	
+
 	@Test
-	public void transposeTest(){
+	public void transposeTest() {
 		IndexedRowMatrix Rt = DistributedMatrixUtilities.transpose(R);
-		double[][] expectedValues = {{1.0d,3.0d},{2.0d,2.0d},{5.0d,4.0d},{2.0d,1.0d}};
-		for(int rowIndex = 0; rowIndex < 4; rowIndex++){
+		double[][] expectedValues = { { 1.0d, 3.0d }, { 2.0d, 2.0d },
+				{ 5.0d, 4.0d }, { 2.0d, 1.0d } };
+		for (int rowIndex = 0; rowIndex < 4; rowIndex++) {
 			final int rowIndexFinal = rowIndex;
-			assertArrayEquals(expectedValues[rowIndex], Rt.rows().toJavaRDD().filter(row -> row.index() == rowIndexFinal).collect().get(0).vector().toArray(),0.0d);
+			assertArrayEquals(
+					expectedValues[rowIndex],
+					Rt.rows().toJavaRDD()
+							.filter(row -> row.index() == rowIndexFinal)
+							.collect().get(0).vector().toArray(), 0.0d);
 		}
 	}
-	
+
 	@Test
-	public void inverseDiagonalMatrixTest(){
-		IndexedRowMatrix inv = DistributedMatrixUtilities.inverseDiagonalMatrix(R);
-		double[] expectedValues = {1.0d, 0.5d};
-		inv.rows().toJavaRDD().foreach(row ->{
-			if(row.index() == 0){
-				assertEquals(expectedValues[0], row.vector().apply(0),0.0d);
-			}
-			else if(row.index() == 1){
-				assertEquals(expectedValues[1], row.vector().apply(1), 0.0d);
-			}
-			for(int i = 0; i < matrixValues[0].length; i++){
-				if(i != row.index()){
-					assertEquals(matrixValues[toIntExact(row.index())][i], row.vector().apply(i),0.0d);
-				}
-			}
-		});
+	public void inverseDiagonalMatrixTest() {
+		IndexedRowMatrix inv = DistributedMatrixUtilities
+				.inverseDiagonalMatrix(R);
+		double[] expectedValues = { 1.0d, 0.5d };
+		inv.rows()
+				.toJavaRDD()
+				.foreach(
+						row -> {
+							if (row.index() == 0) {
+								assertEquals(expectedValues[0], row.vector()
+										.apply(0), 0.0d);
+							} else if (row.index() == 1) {
+								assertEquals(expectedValues[1], row.vector()
+										.apply(1), 0.0d);
+							}
+							for (int i = 0; i < matrixValues[0].length; i++) {
+								if (i != row.index()) {
+									assertEquals(
+											matrixValues[toIntExact(row.index())][i],
+											row.vector().apply(i), 0.0d);
+								}
+							}
+						});
 	}
-	
+
 	@Test
-	public void hardThresholdTest(){
-		IndexedRowMatrix hardThresholdedMat = DistributedMatrixUtilities.hardThreshold(R, 1);
-		hardThresholdedMat.rows().toJavaRDD().foreach(row ->{
-			if(row.index() == 0){
-				assertEquals(0.0d, row.vector().apply(0),0.0d);
-			}
-			for(int i = 0; i < matrixValues[0].length; i++){
-				if(row.index() == 0)
-					i++;
-				assertEquals(matrixValues[toIntExact(row.index())][i], row.vector().apply(i),0.0d);
-			}
-		});
+	public void hardThresholdMatrixTest() {
+		int hardThresholdValue = 1;
+		IndexedRowMatrix hardThresholdedMat = DistributedMatrixUtilities
+				.hardThreshold(R, hardThresholdValue);
+		hardThresholdedMat
+				.rows()
+				.toJavaRDD()
+				.foreach(
+						row -> {
+							if (row.index() == 0) {
+								assertEquals(0.0d, row.vector().apply(0), 0.0d);
+							}
+							for (int i = 0; i < matrixValues[0].length; i++) {
+								if (row.index() == 0)
+									i++;
+								assertEquals(
+										matrixValues[toIntExact(row.index())][i],
+										row.vector().apply(i), 0.0d);
+							}
+						});
 	}
-	
+
+	@Test
+	public void hardThresholdVectorTest() {
+		int hardThresholdValue = 2;
+		Vector hardThresholdedMat = DistributedMatrixUtilities.hardThreshold(
+				Vectors.dense(matrixValues[0]), 2);
+		double[] hardThresholdedValues = hardThresholdedMat.toArray();
+		for (int i = 0; i < hardThresholdedValues.length; i++) {
+			if (i >= hardThresholdValue) {
+				assertEquals(0.0d, hardThresholdedValues[i], 0.0d);
+			} else {
+				assertEquals(matrixValues[0][i], hardThresholdedValues[i], 0.0d);
+			}
+		}
+	}
+
 	@AfterClass
-	public static void tearDownOnce(){
+	public static void tearDownOnce() {
 		sc.close();
 	}
 }
