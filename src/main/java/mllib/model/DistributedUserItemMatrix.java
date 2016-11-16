35b9ae3fb4fb5ff1a9b80b41265a8cc484b0ffd2
Jonathan Bergeron <jonathan.bergeron.6@ulaval.ca>
2016-11-16 09:16:03 -0500

    Commented the DistributedUserItemMatrix.

--- src/main/java/mllib/model/DistributedUserItemMatrix.java
+++ src/main/java/mllib/model/DistributedUserItemMatrix.java
@@ -1,7 +1,6 @@
 package mllib.model;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.apache.spark.api.java.JavaRDD;
@@ -10,12 +9,20 @@ import org.apache.spark.mllib.linalg.distributed.IndexedRow;
 import org.apache.spark.mllib.linalg.distributed.IndexedRowMatrix;
 import org.apache.spark.mllib.linalg.distributed.MatrixEntry;
 
-
+/**
+ * Class that represents an user item matrix (or rating matrix) in a mllib
+ * distributed representation.
+ * 
+ * @author Jonathan Bergeron
+ *
+ */
 public class DistributedUserItemMatrix {
-	
-	
+
+	/**
+	 * The mllib distributed matrix.
+	 */
 	IndexedRowMatrix data;
-	
+
 	/**
 	 * Constructor of the class. Matrix is initialized with zeros.
 	 * 
@@ -27,42 +34,77 @@ public class DistributedUserItemMatrix {
 	public DistributedUserItemMatrix(JavaRDD<IndexedRow> rows) {
 		data = new IndexedRowMatrix(rows.rdd());
 	}
-	
-	public IndexedRow getRow(int rowIndex){
-		List<IndexedRow> rows =  data.rows().toJavaRDD().filter(indexedRow -> indexedRow.index() == rowIndex).collect();
-		if(rows.size() == 1){
+
+	/**
+	 * Method that returns the row of the specified index
+	 * 
+	 * @param rowIndex
+	 *            The row index in the matrix from 0 to numRow-1.
+	 * @return The Indexed Row corresponding to rowIndex.
+	 */
+	public IndexedRow getRow(int rowIndex) {
+		List<IndexedRow> rows = data.rows().toJavaRDD()
+				.filter(indexedRow -> indexedRow.index() == rowIndex).collect();
+		if (rows.size() == 1) {
 			return rows.get(0);
 		}
 		return null;
 	}
-	
-	public double getValue(int rowIndex, int columnIndex){
+
+	/**
+	 * Method that returns the value at specified row index and column index.
+	 * 
+	 * @param rowIndex
+	 *            The row index in the matrix from 0 to numRow-1.
+	 * @param columnIndex
+	 *            The col index in the matrix from 0 to numCol -1.
+	 * @return The value at the specified row index and column index.
+	 */
+	public double getValue(int rowIndex, int columnIndex) {
 		return getRow(rowIndex).vector().toArray()[columnIndex];
 	}
-	
-	public long getNumRows(){
+
+	/**
+	 * Wrapper method that returns the number of rows in the user item matrix.
+	 * 
+	 * @return The number of rows.
+	 */
+	public long getNumRows() {
 		return data.numRows();
 	}
-	
-	public long getNumCols(){
+
+	/**
+	 * Wrapper method that returns the number of cols in the user item matrix.
+	 * 
+	 * @return The number of cols.
+	 */
+	public long getNumCols() {
 		return data.numCols();
 	}
-	
+
 	/**
-	 * Wrapper method that return cosine similarity between columns.
+	 * Wrapper method that return cosine similarity between columns. TODO: It
+	 * may be not necessary to complete the symmetric matrix.
+	 * 
 	 * @return A coordinate matrix containing similarities between columns.
 	 */
-	public CoordinateMatrix getItemSimilarities(){
-		JavaRDD<MatrixEntry> simMat = data.columnSimilarities().entries().toJavaRDD().flatMap(entry -> {
-			List<MatrixEntry> entries = new ArrayList<MatrixEntry>();
-			entries.add(entry);
-			if(entry.i() != entry.j())
-			{
-				entries.add(new MatrixEntry(entry.j(), entry.i(), entry.value()));
-			}
-			return entries.iterator();
-		});
-		CoordinateMatrix fullSpecifiedItemSim = new CoordinateMatrix(simMat.rdd(), data.numCols(), data.numCols());
+	public CoordinateMatrix getItemSimilarities() {
+		JavaRDD<MatrixEntry> simMat = data
+				.columnSimilarities()
+				.entries()
+				.toJavaRDD()
+				.flatMap(
+						entry -> {
+							List<MatrixEntry> entries = new ArrayList<MatrixEntry>();
+							entries.add(entry);
+							if (entry.i() != entry.j()) {
+								entries.add(new MatrixEntry(entry.j(), entry
+										.i(), entry.value()));
+							}
+							return entries.iterator();
+						});
+		CoordinateMatrix fullSpecifiedItemSim = new CoordinateMatrix(
+				simMat.rdd(), data.numCols(), data.numCols());
 		return fullSpecifiedItemSim;
 	}
 }
