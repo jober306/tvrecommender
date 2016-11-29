3baa02fbf88e15151e24d316d37de2256ca3dfbe
H. C <Hamidreza.Chinaei.1@ulaval.ca>
2016-11-29 12:51:42 -0600

    Added getter methods to the user preference tensor and overrided equals and hashcode methods.

--- src/main/java/mllib/model/tensor/UserPreferenceTensor.java
+++ src/main/java/mllib/model/tensor/UserPreferenceTensor.java
@@ -1,5 +1,7 @@
 package mllib.model.tensor;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.spark.mllib.linalg.Vector;
 
 public class UserPreferenceTensor {
@@ -19,5 +21,37 @@ public class UserPreferenceTensor {
 	
 	public void IncrementValue(int watchTime){
 		totalWatchTime += watchTime;
+	}
+	
+	public int getUserId(){
+		return userId;
+	}
+	
+	public Vector getProgramFeatureVector(){
+		return programFeatureVector;
+	}
+	
+	public short getSlot(){
+		return slot;
+	}
+	
+	@Override
+    public int hashCode() {
+		return new HashCodeBuilder(17,31).append(userId).append(programFeatureVector.toArray()).append(slot).hashCode();
+	}
+	
+	@Override
+	public boolean equals(Object obj){
+		if (!(obj instanceof UserPreferenceTensor))
+            return false;
+        if (obj == this)
+            return true;
+
+        UserPreferenceTensor rhs = (UserPreferenceTensor) obj;
+        return new EqualsBuilder().
+            append(userId, rhs.getUserId()).
+            append(programFeatureVector.toArray(), rhs.getProgramFeatureVector().toArray()).
+            append(slot, rhs.getSlot()).
+            isEquals();
 	}
 }
