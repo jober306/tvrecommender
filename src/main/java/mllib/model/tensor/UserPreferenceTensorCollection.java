36e97637412b9ce843ca9c1a158bd2fd27e60871
H. C <Hamidreza.Chinaei.1@ulaval.ca>
2016-11-29 14:09:50 -0600

    Implemented the UserPreferenceTensorCollection and commented it.

--- src/main/java/mllib/model/tensor/UserPreferenceTensorCollection.java
+++ src/main/java/mllib/model/tensor/UserPreferenceTensorCollection.java
@@ -2,8 +2,35 @@ package mllib.model.tensor;
 
 import java.util.HashMap;
 
-public class UserPreferenceTensorCollection extends HashMap<UserPreferenceTensor, Integer>{
+/**
+ * Class that represents a collection of user preference tensor that extends hash map. The main advantage is when using
+ * the put method the time value of the new user preference tensor will be added if one was already existing with same
+ * user id, program feature vector and slot. It also offers utilities method.
+ * @author Jonathan Bergeron
+ *
+ */
+public class UserPreferenceTensorCollection extends HashMap<UserPreferenceTensor,UserPreferenceTensor>{
 
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Default constructor that build an empty HashMap of UserPreferenceTensor.
+	 */
+	public UserPreferenceTensorCollection(){
+		super();
+	}
+	
+	@Override
+	public UserPreferenceTensor put(UserPreferenceTensor key, UserPreferenceTensor value){
+		if(containsKey(key)){
+			UserPreferenceTensor current  = get(key);
+			UserPreferenceTensor old = new UserPreferenceTensor(current.getUserId(), current.getProgramFeatureVector(), current.getSlot());
+			current.IncrementValue(value.getTotalWatchTime());
+			return old;
+		}else{
+			put(key, value);
+			return value;
+		}
+	}
 
 }
