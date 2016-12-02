019ced8fe48f1706bfb0be4a8babd674f195c08a
H. C <Hamidreza.Chinaei.1@ulaval.ca>
2016-12-01 19:33:36 -0600

    Added a general getter to the user preference tensor collection that allow to get tensors with specific or general attributes. Tested the whole class.

--- src/main/java/mllib/model/tensor/UserPreferenceTensorCollection.java
+++ src/main/java/mllib/model/tensor/UserPreferenceTensorCollection.java
@@ -1,9 +1,15 @@
 package mllib.model.tensor;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.linalg.Vectors;
 
 /**
  * Class that represents a collection of user preference tensor that extends hash map. The main advantage is when using
@@ -13,6 +19,14 @@ import java.util.Map.Entry;
  *
  */
 public class UserPreferenceTensorCollection {
+	
+	public static final int ANY = -1;
+	
+	public static Vector getAnyFeatureVector(int size){
+		double[] features = new double[size];
+		Arrays.fill(features, ANY);
+		return Vectors.dense(features);
+	}
 	
 	/**
 	 * The synchronised map containing the user preference tensor. The total watch time of a specific
@@ -34,6 +48,46 @@ public class UserPreferenceTensorCollection {
 	public UserPreferenceTensorCollection(UserPreferenceTensorCollection c){
 		syncMap = Collections.synchronizedMap(c.getSyncMap());
 	}
+	
+	public List<UserPreferenceTensor> getAllUserPreferenceTensors(){
+		return syncMap.entrySet().stream().map(Entry::getValue).collect(Collectors.toList());
+	}
+	
+	/**
+	 * Method that returns all the user preference tensors with the corresponding user id,
+	 * program feature vectors and slot. It is also possible to give the
+	 * UserPreferenceTensorCollection.ANY value to any of the arguments, including every
+	 * value in the program feature vector. For example if the ANY value is given for user
+	 * id, the returned list will contains tensors corresponding to the given slot and program
+	 * feature vector for any users.
+	 * @param userId The wanted user id.
+	 * @param programFeatures The wanted program feature vector.
+	 * @param slot The wanted slot.
+	 * @return A list containing all the user preference tensors corresponding to the given attributes.
+	 */
+	public List<UserPreferenceTensor> getUserPreferenceTensors(int userId, Vector programFeatures, int slot){
+		final double[] wantedProgramFeatures = programFeatures.toArray();
+		return syncMap.entrySet().stream().filter(entry -> {
+			UserPreferenceTensor tensor = entry.getValue();
+			if(userId != ANY && userId != tensor.getUserId()){
+				return false;
+			}
+			double[] tensorFeatures = tensor.getProgramFeatureVector().toArray();
+			for(int i = 0; i < tensorFeatures.length; i++){
+				double feature = tensorFeatures[i];
+				double wantedFeature = wantedProgramFeatures[i];
+				if(wantedFeature != ANY && wantedFeature != feature){
+					return false;
+				}
+			}
+			if(slot != ANY && slot != tensor.getSlot()){
+				return false;
+			}
+			return true;
+		}).map(Entry::getValue).collect(Collectors.toList());
+	}
+	
+	
 	
 	/**
 	 * Method to add the watching time of this tensor to the corresponding tensor in the map. If
