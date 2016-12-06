e988d9d86d915e7a682799c42ac433143e6a3b06
H. C <Hamidreza.Chinaei.1@ulaval.ca>
2016-12-06 14:58:53 -0600

    Added tests to the user preference tensor calculator.

--- src/test/java/mllib/model/tensor/UserPrefenreceTensorCalculatorTest.java
+++ src/test/java/mllib/model/tensor/UserPrefenreceTensorCalculatorTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 import static mllib.model.tensor.UserPreferenceTensorCollection.ANY;
 import static mllib.model.tensor.UserPreferenceTensorCollection.getAnyFeatureVector;
 
+import org.apache.spark.mllib.linalg.Vectors;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -15,6 +16,7 @@ import org.junit.Test;
 import data.recsys.loader.RecsysTVDataSetLoader;
 import data.recsys.model.RecsysTVDataSet;
 import data.recsys.model.RecsysTVEvent;
+import scala.annotation.implicitNotFound;
 
 public class UserPrefenreceTensorCalculatorTest {
 	
@@ -35,6 +37,12 @@ public class UserPrefenreceTensorCalculatorTest {
 		UserPreferenceTensorCollection tensors = calculator.calculateUserPreferenceTensorForDataSet(dataSet);
 		List<UserPreferenceTensor> user1Tensors = tensors.getUserPreferenceTensors(1, getAnyFeatureVector(4), ANY);
 		assertEquals(3, user1Tensors.size());
+		int expectedWatchTime = 61;
+		int actualTotalWatchTime = user1Tensors.stream().mapToInt(tensor -> tensor.getTotalWatchTime()).sum();
+		assertEquals(expectedWatchTime, actualTotalWatchTime);
+		int expectedTotalWatchTimeUser2 = 6;
+		int actualTotalWatchTimeUser2 = tensors.getUserPreferenceTensors(2, Vectors.dense(new double[]{46,19,5,81}), 19).get(0).getTotalWatchTime();
+		assertEquals(expectedTotalWatchTimeUser2, actualTotalWatchTimeUser2);
 	}
 	
 	@AfterClass
