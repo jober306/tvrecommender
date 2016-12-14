05bf8875108c92cf42ccc0e9cd87eefb4784846b
H. C <Hamidreza.Chinaei.1@ulaval.ca>
2016-12-14 13:46:53 -0600

    Added utilities method to filter out events that are not in a time windows.

--- src/main/java/data/utility/TVDataSetUtilities.java
+++ src/main/java/data/utility/TVDataSetUtilities.java
@@ -1,5 +1,10 @@
 package data.utility;
 
+import static time.utilities.TimeUtilities.*;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 import org.apache.spark.api.java.JavaRDD;
 
 import data.model.TVEvent;
@@ -22,5 +27,28 @@ public class TVDataSetUtilities {
 	 */
 	public static <T extends TVEvent> JavaRDD<T> filterByMinTimeView(JavaRDD<T> events, int minTimeView) {
 		return events.filter(tvEvent -> tvEvent.getDuration() >= minTimeView);
+	}
+	
+	/**
+	 * Method that returns tv events that have been started watching between start time and end time inclusively.
+	 * @param events All the tv events.
+	 * @param startTime The start time.
+	 * @param endTime The end time.
+	 * @return The tv events between startTime and endTime.
+	 */
+	public static <T extends TVEvent> JavaRDD<T> filterByDateTime(JavaRDD<T> events, LocalDateTime startTime,LocalDateTime endTime ){
+		return events.filter(tvEvent -> isDateTimeBetweenInclusive(startTime, endTime, tvEvent.getWatchTime()));
+	}
+	
+	/**
+	 * Method that returns tv events that have been started watching between start time and end time inclusively ignoring
+	 * the date. For instance it could filter out all the events that are not between 12h00 and 16h30.
+	 * @param events All the tv events.
+	 * @param startTime The start time.
+	 * @param endTime The end time.
+	 * @return The tv events between startTime and endTime.
+	 */
+	public static <T extends TVEvent> JavaRDD<T> filterByTime(JavaRDD<T> events, LocalTime startTime,LocalTime endTime){
+		return events.filter(tvEvent -> isTimeBetweenInclusive(startTime, endTime, tvEvent.getWatchTime().toLocalTime()));
 	}
 }
--- src/main/java/time/utilities/TimeUtilities.java
+++ src/main/java/time/utilities/TimeUtilities.java
@@ -1,10 +1,15 @@
 package time.utilities;
 
+import java.time.LocalTime;
 import java.time.chrono.ChronoLocalDateTime;
 
 public class TimeUtilities {
 	
-	public static boolean isBetweenInclusive(ChronoLocalDateTime<?> startTime, ChronoLocalDateTime<?> endTime, ChronoLocalDateTime<?> targetTime){
+	public static boolean isDateTimeBetweenInclusive(ChronoLocalDateTime<?> startTime, ChronoLocalDateTime<?> endTime, ChronoLocalDateTime<?> targetTime){
+		return !targetTime.isBefore(startTime) && !targetTime.isAfter(endTime);
+	}
+	
+	public static boolean isTimeBetweenInclusive(LocalTime startTime, LocalTime endTime, LocalTime targetTime){
 		return !targetTime.isBefore(startTime) && !targetTime.isAfter(endTime);
 	}
 }
