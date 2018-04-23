ad9db61a4d064d6fdf3d114b4930e73fc5eb35ff
Jonathan Bergeron <jonathan.bergeron.6@ulaval.ca>
2018-04-23 13:00:30 -0400

    Commented the CartesianProduct2 and 3 classes.

--- src/main/java/util/collections/CartesianProduct2.java
+++ src/main/java/util/collections/CartesianProduct2.java
@@ -6,6 +6,15 @@ import java.util.stream.Stream;
 
 import scala.Tuple2;
 
+/**
+ * A class that generates the cartesian product between two lists.
+ * This class was created because the method of the same name in ListUtilities contains flatMap
+ * which is a terminal operation breaking the lazy behavior. This generator is truly lazy for all elements.
+ * @author Jonathan Bergeron
+ *
+ * @param <A> The type of first list
+ * @param <B> The type of the second list
+ */
 public class CartesianProduct2<A, B> implements Supplier<Tuple2<A, B>>{
 	
 	final List<A> listA;
@@ -14,6 +23,11 @@ public class CartesianProduct2<A, B> implements Supplier<Tuple2<A, B>>{
 	final List<B> listB;
 	int indexB;
 	
+	/**
+	 * Constructor of this generator
+	 * @param listA The first list
+	 * @param listB The second list
+	 */
 	public CartesianProduct2(List<A> listA, List<B> listB){
 		this.listA = listA;
 		this.listB = listB;
@@ -31,10 +45,18 @@ public class CartesianProduct2<A, B> implements Supplier<Tuple2<A, B>>{
 		return tuple;
 	}
 	
+	/**
+	 * Method that calculate the cartesian prodcut size.
+	 * @return The cartesian product size.
+	 */
 	public int cartesianProductSize(){
 		return listA.size() * listB.size();
 	}
 	
+	/**
+	 * Method that returns the stream representing the cartesian product of the two lists. 
+	 * @return The stream containing tuple2 of elements of all lists.
+	 */
 	public Stream<Tuple2<A, B>> generate(){
 		return Stream.generate(this).limit(cartesianProductSize());
 	}
--- src/main/java/util/collections/CartesianProduct3.java
+++ src/main/java/util/collections/CartesianProduct3.java
@@ -6,6 +6,16 @@ import java.util.stream.Stream;
 
 import scala.Tuple3;
 
+/**
+ * A class that generates the cartesian product between three lists.
+ * This class was created because the method of the same name in ListUtilities contains flatMap
+ * which is a terminal operation breaking the lazy behavior. This generator is truly lazy for all elements.
+ * @author Jonathan Bergeron
+ *
+ * @param <A> The type of first list
+ * @param <B> The type of the second list
+ * @param <C> The type of the third list
+ */
 public class CartesianProduct3<A, B, C> implements Supplier<Tuple3<A, B, C>>{
 	
 	final List<A> listA;
@@ -17,6 +27,12 @@ public class CartesianProduct3<A, B, C> implements Supplier<Tuple3<A, B, C>>{
 	final List<C> listC;
 	int indexC;
 	
+	/**
+	 * Constructor of this generator
+	 * @param listA The first list
+	 * @param listB The second list
+	 * @param listC the third list
+	 */
 	public CartesianProduct3(List<A> listA, List<B> listB, List<C> listC){
 		this.listA = listA;
 		this.listB = listB;
@@ -39,10 +55,18 @@ public class CartesianProduct3<A, B, C> implements Supplier<Tuple3<A, B, C>>{
 		return tuple;
 	}
 	
+	/**
+	 * Method that calculate the cartesian prodcut size.
+	 * @return The cartesian product size.
+	 */
 	public int cartesianProductSize(){
 		return listA.size() * listB.size() * listC.size();
 	}
 	
+	/**
+	 * Method that returns the stream representing the cartesian product of the three lists. 
+	 * @return The stream containing tuple3 of elements of all lists.
+	 */
 	public Stream<Tuple3<A, B, C>> generate(){
 		return Stream.generate(this).limit(cartesianProductSize());
 	}
