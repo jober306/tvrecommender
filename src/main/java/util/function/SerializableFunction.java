10d93bafde5a7e99bc30273d6381ef76b6c90cd6
Jonathan Bergeron <jonathan.bergeron.6@ulaval.ca>
2018-04-23 13:02:10 -0400

    Commented the SerializableFunction interface.

--- src/main/java/util/function/SerializableFunction.java
+++ src/main/java/util/function/SerializableFunction.java
@@ -3,5 +3,12 @@ package util.function;
 import java.io.Serializable;
 import java.util.function.Function;
 
+/**
+ * class the wraps the java 8 Function interface and is serializable
+ * @author Jonathan Bergeron
+ *
+ * @param <T> The input of the function
+ * @param <R> The output of the function
+ */
 @FunctionalInterface
 public interface SerializableFunction<T, R> extends Function<T, R>, Serializable{}
